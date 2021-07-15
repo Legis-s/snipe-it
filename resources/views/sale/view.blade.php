d327be3dded958aa248bfe767aeb366e2d8b74ce
Sergey Markin <okella92@gmail.com>
2021-07-15 12:07:34 +0300

    пнечать на принтере

--- resources/views/sale/view.blade.php
+++ resources/views/sale/view.blade.php
@@ -873,10 +873,10 @@
         $.ajax({
           type: "POST",
           url: "http://127.0.0.1:8001/termal_print",
-          dataType: 'json',
-          contentType: 'application/json',
+          // dataType: 'json',
+          // contentType: 'application/json',
           data: JSON.stringify(dataToSend),
-          crossDomain: true,
+          // crossDomain: true,
           headers: {
             'Access-Control-Allow-Origin': '*',
           },
