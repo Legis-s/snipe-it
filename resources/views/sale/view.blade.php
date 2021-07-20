bd8a7c47f829986666762742a2f87e40fb88cf05
Sergey Markin <okella92@gmail.com>
2021-07-20 15:49:00 +0300

    тип и статаус договора

--- resources/views/sale/view.blade.php
+++ resources/views/sale/view.blade.php
@@ -867,76 +867,76 @@
 
       $('#print_tag').click(function() {
         console.log("test");
-        var dataToSend = {
-          text: "{{ $sale->asset_tag }}"
-        };
-        // $.ajax('http://localhost:8001/termal_print', {
-        //   success: function (data, textStatus, xhr) {
-        //     console.log(xhr.status);
-        //     if (xhr.status === 200) {
-        //       console.log(data);
-        //     } else {
-        //       console.log(data);
-        //     }
-        //   },
-        //   error: function () {
-        //     console.log("error");
-        //   }
-        // });
-        $.ajax({
-          type: "GET",
-          url: "http://localhost:8001/termal_print",
-          // dataType: 'json',
-          // contentType: 'application/json',
-          data: JSON.stringify(dataToSend),
-          // crossDomain: true,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-          },
-          success: function(data, textStatus, xhr){
-            if (xhr.status == 200){
-              $.notify({
-                // options
-                title: "Успешно",
-                // message: 'Ошибка'
-              },{
-                // settings
-                type: 'info',
-                placement: {
-                  from: "bottom",
-                  align: "right"
-                },
-              });
-            }else{
-              $.notify({
-                // options
-                title: "Ошибка",
-                // message: 'Ошибка'
-              },{
-                // settings
-                type: 'danger',
-                placement: {
-                  from: "bottom",
-                  align: "right"
-                },
-              });
+        {{--var dataToSend = {--}}
+        {{--  text: "{{ $sale->asset_tag }}"--}}
+        {{--};--}}
+        $.ajax('http://localhost: 8001/termal_print?text={{ $sale->asset_tag }}', {
+          success: function (data, textStatus, xhr) {
+            console.log(xhr.status);
+            if (xhr.status === 200) {
+              console.log(data);
+            } else {
+              console.log(data);
             }
           },
-          error: function(xhr, textStatus){
-            $.notify({
-              // options
-              title: "Ошибка",
-              message: textStatus
-            },{
-              // settings
-              type: 'danger',
-              placement: {
-                from: "bottom",
-                align: "right"
-              },
-            });
+          error: function () {
+            console.log("error");
           }
         });
+        // $.ajax({
+        //   type: "GET",
+        //   url: "http://localhost:8001/termal_print",
+        //   // dataType: 'json',
+        //   // contentType: 'application/json',
+        //   data: JSON.stringify(dataToSend),
+        //   // crossDomain: true,
+        //   headers: {
+        //     'Access-Control-Allow-Origin': '*',
+        //   },
+        //   success: function(data, textStatus, xhr){
+        //     if (xhr.status == 200){
+        //       $.notify({
+        //         // options
+        //         title: "Успешно",
+        //         // message: 'Ошибка'
+        //       },{
+        //         // settings
+        //         type: 'info',
+        //         placement: {
+        //           from: "bottom",
+        //           align: "right"
+        //         },
+        //       });
+        //     }else{
+        //       $.notify({
+        //         // options
+        //         title: "Ошибка",
+        //         // message: 'Ошибка'
+        //       },{
+        //         // settings
+        //         type: 'danger',
+        //         placement: {
+        //           from: "bottom",
+        //           align: "right"
+        //         },
+        //       });
+        //     }
+        //   },
+        //   error: function(xhr, textStatus){
+        //     $.notify({
+        //       // options
+        //       title: "Ошибка",
+        //       message: textStatus
+        //     },{
+        //       // settings
+        //       type: 'danger',
+        //       placement: {
+        //         from: "bottom",
+        //         align: "right"
+        //       },
+        //     });
+        //   }
+        // });
 
       });
       $('#assetHistory').on('post-body.bs.table', function (e, data) {
