daf3a72d8e935f45a4f5f819f4301173a922ac3a
Sergey Markin <okella92@gmail.com>
2021-09-06 19:01:32 +0300

    проданные активы в договоре

--- app/Console/Commands/Migration.php
+++ app/Console/Commands/Migration.php
@@ -2,9 +2,16 @@
 
 namespace App\Console\Commands;
 
+use App\Helpers\Helper;
+use App\Models\Actionlog;
+use App\Models\Asset;
+use App\Models\AssetModel;
+use App\Models\Company;
 use App\Models\ConsumableAssignment;
 use App\Models\Purchase;
+use App\Models\Sale;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 
 class Migration extends Command
@@ -41,29 +48,57 @@ class Migration extends Command
     public function handle()
     {
 
-        $purchases = Purchase::where('status', "paid")->whereRaw('LENGTH(consumables_json) > 3')->get();;
-        $assigned_type = "App\Models\Purchase";
-        foreach ($purchases as $purchase) {
-//            $this->info($purchase->consumables_json);
-            $consumables =$purchase->consumables;
-            if (count($consumables)>0){
-                foreach ($consumables as $consumable) {
-                    $consumable->locations()->attach($consumable->id, [
-                        'consumable_id' => $consumable->id,
-                        'user_id' =>$purchase->user_id,
-                        'quantity' => $consumable->qty,
-                        'cost' => $consumable->purchase_cost,
-                        'type' => ConsumableAssignment::PURCHASE,
-                        'assigned_to' => $purchase->id,
-                        'assigned_type' => $assigned_type,
-                        'created_at' => $purchase->created_at,
-                        'updated_at' => $purchase->created_at,
-                    ]);
+        $sales = Sale::get();;
+        foreach ($sales as $sale) {
+            $this->info($sale);
+            $asset = new Asset();
+            $asset->model()->associate(AssetModel::find((int)$sale->model_id));
 
-                    $this->info($consumable);
-                }
+            $asset->name = $sale->name;
+            $asset->serial =  $sale->serial;;
+            $asset->model_id =  $sale->model_id;
+            $asset->order_number = $sale->order_number;
+            $asset->notes = $sale->notes;
+            $asset->asset_tag = $sale->asset_tag;
+            $asset->user_id =  $sale->user_id;
+            $asset->status_id = $sale->status_id;
+            $asset->purchase_cost = $sale->purchase_cost;
+            $asset->purchase_date = $sale->purchase_date;
+            $asset->supplier_id = $sale->supplier_id;
+            $asset->physical = $sale->physical;
+            $asset->location_id = $sale->location_id;
+            $asset->purchase_id = $sale->purchase_id;
+            $asset->purchase_cost = $sale->purchase_cost;
+            $asset->nds = $sale->nds;
+            $asset->user_verified_id = $sale->user_verified_id;
+            $asset->contract_id = $sale->contract_id;
+            $asset->quality = 5;
+            $asset->save();
+            if($sale->closing_documents && $sale->contract_id>0){
+                $log = new Actionlog();
+                $log->user_id = $sale->user_id;
+                $log->action_type = 'sell';
+                $log->target_type = "App\Models\Contract";
+                $log->target_id = $asset->contract_id;
+                $log->item_id = $asset->id;
+                $log->item_type = Asset::class;
+                $log->save();
+                $asset->assigned_type = "App\Models\Contract";
+                $asset->assigned_to = $sale->contract_id;
+            }else if ($sale->user_responsible_id>0){
+                $log = new Actionlog();
+                $log->user_id = $sale->user_id;
+                $log->action_type = 'issued_for_sale';
+                $log->target_type = "App\Models\User";
+                $log->target_id = $sale->user_responsible_id;
+                $log->item_id = $asset->id;
+                $log->item_type = Asset::class;
+                $log->save();
+                $asset->assigned_type = "App\Models\User";
+                $asset->assigned_to = $sale->user_responsible_id;
             }
 
+            $sale->delete();
         }
     }
 }
