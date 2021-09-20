8266527a040f54406a7aa2848896cb490f8c077a
Sergey Markin <okella92@gmail.com>
2021-09-20 15:15:11 +0300

    print label button

--- app/Http/Controllers/Api/BiometricController.php
+++ app/Http/Controllers/Api/BiometricController.php
@@ -24,10 +24,11 @@ class BiometricController extends Controller
         $client = new \GuzzleHttp\Client();
 
         $response = $client->request('GET', 'https://tabelapi.legis-s.ru:45443/tabel_local_server/response/'.$id, [
+            'allow_redirects'=>true,
             'headers' => [
                 'Authorization' => 'TabelLocalServerToken gAAAAABgkwAkYnPxsSkFvUGu51MshvJ1dzBlxhdzcpxm34n7Q_zalcEazo7dPWfWhO9ouhDqrdLH3O-cQFKwoD6QpGNwkv_V-M4Eit1880xt0R83RPGIeyl3MrBWcQhl-sYvR1b6uBo3',
             ]
-            ]);
+        ]);
         $responseJson = $response->getBody()->getContents();
 
         return $responseJson;
