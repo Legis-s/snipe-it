41051bf31ca545dbea069a89dca488a6339b4404
Sergey Markin <okella92@gmail.com>
2021-11-29 19:02:07 +0300

    добавлена синхронизация пультовых объектов

--- resources/views/contracts/view.blade.php
+++ resources/views/contracts/view.blade.php
@@ -44,32 +44,6 @@
             <div class="box box-default">
                 <div class="box-header with-border">
                     <div class="box-heading">
-                        <h2 class="box-title">Активы на продажу</h2>
-                    </div>
-                </div>
-                <div class="box-body">
-                    <div class="table table-responsive">
-                        <table
-                                data-columns="{{ \App\Presenters\SalesPresenter::dataTableLayout() }}"
-                                data-cookie-id-table="assetsListingTable"
-                                data-pagination="true"
-                                data-id-table="assetsListingTable"
-                                data-search="true"
-                                data-side-pagination="server"
-                                data-show-columns="true"
-                                data-show-export="true"
-                                data-show-refresh="true"
-                                data-sort-order="asc"
-                                id="assetsListingTable"
-                                class="table table-striped snipe-table"
-                                data-url="{{route('api.sales.index', ['contract_id' => $contract->id]) }}">
-                        </table>
-                    </div><!-- /.table-responsive -->
-                </div><!-- /.box-body -->
-            </div> <!--/.box-->
-            <div class="box box-default">
-                <div class="box-header with-border">
-                    <div class="box-heading">
                         <h2 class="box-title">Расходники на продажу</h2>
                     </div>
                 </div>
