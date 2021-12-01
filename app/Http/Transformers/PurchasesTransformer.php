c5a84a508937747010aa6c18416c7e453e08487d
Sergey Markin <okella92@gmail.com>
2021-12-01 15:00:16 +0300

    добавлено тех. безопасность в местоположения

--- app/Http/Transformers/PurchasesTransformer.php
+++ app/Http/Transformers/PurchasesTransformer.php
@@ -64,7 +64,7 @@ class PurchasesTransformer
             'clone' => (Gate::allows('create', Purchase::class) && ($purchase->deleted_at == '')),
         ];
         if ($full) {
-            $array += ['consumables_json' => ($purchase->consumables_json) ? e($purchase->consumables_json) : null];
+            $array += ['consumables_json' => ($purchase->consumables_json) ? e($consumables) : null];
         }
         $array += $permissions_array;
 
