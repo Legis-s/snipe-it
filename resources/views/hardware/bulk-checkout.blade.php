e13a1893b460ff01a7e014f8a1a4a73ea906c228
Sergey Markin <okella92@gmail.com>
2022-04-26 22:33:56 +0300

    массовая выдача

--- resources/views/hardware/bulk-checkout.blade.php
+++ resources/views/hardware/bulk-checkout.blade.php
@@ -18,23 +18,23 @@
 
     <div class="row">
         <!-- left column -->
-        <div class="col-md-7">
+        <div class="col-md-9">
             <div class="box box-default">
                 <div class="box-header with-border">
                     <h2 class="box-title"> Массовая выдача активов </h2>
                 </div>
-                <div class="box-body">
-                    <form class="form-horizontal" method="post" action="" autocomplete="off">
-                    {{ csrf_field() }}
+                <form class="form-horizontal" method="post" action="" autocomplete="off">
+                    <div class="box-body">
+                        {{ csrf_field() }}
 
-                    <!-- Checkout selector -->
-                    @include ('partials.forms.checkout-selector', ['user_select' => 'true','asset_select' => 'true', 'location_select' => 'true'])
+                        <!-- Checkout selector -->
+                        @include ('partials.forms.checkout-selector', ['user_select' => 'true','asset_select' => 'true', 'location_select' => 'true'])
 
-                    @include ('partials.forms.edit.user-select', ['translated_name' => trans('general.user'), 'fieldname' => 'assigned_user', 'required'=>'true'])
-                    @include ('partials.forms.edit.asset-select', ['translated_name' => trans('general.asset'), 'fieldname' => 'assigned_asset', 'unselect' => 'true', 'style' => 'display:none;', 'required'=>'true'])
-                    @include ('partials.forms.edit.location-select', ['translated_name' => trans('general.location'), 'fieldname' => 'assigned_location', 'style' => 'display:none;', 'required'=>'true'])
+                        @include ('partials.forms.edit.location-select', ['translated_name' => trans('general.location'), 'fieldname' => 'assigned_location', 'required'=>'true'])
+                        @include ('partials.forms.edit.user-select',     ['translated_name' => trans('general.user'),     'fieldname' => 'assigned_user', 'unselect' => 'true', 'style' => 'display:none;', 'required'=>'true'])
+                        @include ('partials.forms.edit.asset-select',    ['translated_name' => trans('general.asset'),    'fieldname' => 'assigned_asset', 'unselect' => 'true', 'style' => 'display:none;', 'required'=>'true'])
 
-                    <!-- Checkout/Checkin Date -->
+                        <!-- Checkout/Checkin Date -->
                         <div class="form-group {{ $errors->has('checkout_at') ? 'error' : '' }}">
                             {{ Form::label('checkout_at', trans('admin/hardware/form.checkout_date'), array('class' => 'col-md-3 control-label')) }}
                             <div class="col-md-8">
@@ -71,25 +71,48 @@
                         <div class="form-group {{ $errors->has('note') ? 'error' : '' }}">
                             {{ Form::label('note', trans('admin/hardware/form.notes'), array('class' => 'col-md-3 control-label')) }}
                             <div class="col-md-8">
-                                <textarea class="col-md-6 form-control" id="note"
-                                          name="note">{{ Input::old('note') }}</textarea>
+                                                        <textarea class="col-md-6 form-control" id="note"
+                                                                  name="note">{{ Input::old('note') }}</textarea>
                                 {!! $errors->first('note', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
                             </div>
                         </div>
 
+
+                        <br>
+
+                        <br>
                         <!-- We have to pass unselect here so that we don't default to the asset that's being checked out. We want that asset to be pre-selected everywhere else. -->
-                        @include ('partials.forms.edit.asset-select', ['translated_name' => trans('general.asset'), 'fieldname' => 'asset_select', 'unselect' => 'true'])
+                        @include ('partials.forms.edit.asset-select-bulk-form', [
+                                    'translated_name' => trans('general.asset'),
+                                    'fieldname' => 'asset_select',
+                                     'unselect' => 'true',
+                                     'asset_status_type' => 'RTD',
+                                     'select_id' => 'asset_select',
+                                     ])
+
+                        <!-- Purchase Cost -->
+                        <style>
+                            @import 'star-rating';
+
+                            :root {
+                                --gl-star-empty: url(/img/star-empty.svg);
+                                --gl-star-full: url(/img/star-full.svg);
+                                --gl-star-size: 32px;
+                            }
+                        </style>
 
 
-{{--                        @include ('partials.forms.edit.asset-select', [--}}
-{{--                          'translated_name' => trans('general.assets'),--}}
-{{--                          'fieldname' => 'selected_assets[]',--}}
-{{--                          'multiple' => true,--}}
-{{--                          'asset_status_type' => 'RTD',--}}
-{{--                          'select_id' => 'assigned_assets_select',--}}
-{{--                        ])--}}
+                        <select id="assigned_assets_select" name="selected_assets[]" multiple hidden></select>
 
+                        {{--                        @include ('partials.forms.edit.asset-select', [--}}
+                        {{--                          'translated_name' => trans('general.assets'),--}}
+                        {{--                          'fieldname' => 'selected_assets[]',--}}
+                        {{--                          'multiple' => true,--}}
+                        {{--                          'asset_status_type' => 'RTD',--}}
+                        {{--                          'select_id' => 'assigned_assets_select',--}}
+                        {{--                        ])--}}
 
+                        <h2> Активы к выдаче:</h2>
                         <table
                                 data-advanced-search="true"
                                 data-click-to-select="true"
@@ -108,135 +131,157 @@
                                 data-queryParams="#toolbar"
                                 id="assetsBulkTable"
                                 class="table table-striped snipe-table"
-                                data-url="{{ route('api.assets.index',
-                    array('bulk' => true ))}}">
+                                data-url="{{ route('api.assets.index',array('bulk' => true ))}}">
                         </table>
 
-
-                </div> <!--./box-body-->
-                <div class="box-footer">
-                    <a class="btn btn-link" href="{{ URL::previous() }}"> {{ trans('button.cancel') }}</a>
-                    <button type="submit" class="btn btn-primary pull-right"><i class="fa fa-check icon-white"
-                                                                                aria-hidden="true"></i> {{ trans('general.checkout') }}
-                    </button>
-                </div>
-            </div>
-            </form>
-        </div> <!--/.col-md-7-->
-
-        <!-- right column -->
-        <div class="col-md-5" id="current_assets_box" style="display:none;">
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h2 class="box-title">{{ trans('admin/users/general.current_assets') }}</h2>
-                </div>
-                <div class="box-body">
-                    <div id="current_assets_content">
+                    </div> <!--./box-body-->
+                    <div class="box-footer">
+                        <a class="btn btn-link" href="{{ URL::previous() }}"> {{ trans('button.cancel') }}</a>
+                        <button type="submit" class="btn btn-primary pull-right"><i class="fa fa-check icon-white"
+                                                                                    aria-hidden="true"></i> {{ trans('general.checkout') }}
+                        </button>
                     </div>
-                </div>
-            </div>
+                </form>
+            </div> <!--/.col-md-7-->
+
+            <!-- right column -->
+            {{--            <div class="col-md-5" id="current_assets_box" style="display:none;">--}}
+            {{--                <div class="box box-primary">--}}
+            {{--                    <div class="box-header with-border">--}}
+            {{--                        <h2 class="box-title">{{ trans('admin/users/general.current_assets') }}</h2>--}}
+            {{--                    </div>--}}
+            {{--                    <div class="box-body">--}}
+            {{--                        <div id="current_assets_content">--}}
+            {{--                        </div>--}}
+            {{--                    </div>--}}
+            {{--                </div>--}}
+            {{--            </div>--}}
         </div>
-    </div>
-@stop
+        @stop
 
-@section('moar_scripts')
-    @include('partials.bootstrap-table')
-    @include('partials/assets-assigned')
-    <script src="{{ asset('js/onscan.js') }}"></script>
-    <script nonce="{{ csrf_token() }}">
-        var select = $('#assigned_assets_select');
-        var selected = [];
-        // Enable scan events for the entire document
-        onScan.attachTo(document, {
-            suffixKeyCodes: [13], // enter-key expected at the end of a scan
-            // reactToPaste: true, // Compatibility to built-in scanners in paste-mode (as opposed to keyboard-mode)
-            onScan: function (sCode, iQty) { // Alternative to document.addEventListener('scan')
-                scanCode(auto_layout_keyboard(sCode));
-            },
-            // onKeyDetect: function(iKeyCode){ // output all potentially relevant key events - great for debugging!
-            //     console.log('Pressed: ' + iKeyCode);
-            // }
-        });
-        function select2_search ($el, term) {
-            $el.select2('open');
+        @section('moar_scripts')
+            @include('partials.bootstrap-table')
+            @include('partials/assets-assigned')
+            <script src="{{ asset('js/onscan.js') }}"></script>
+            <script nonce="{{ csrf_token() }}">
+                var select = $('#asset_select');
+                var add_asset = $('#add_asset');
+                var assigned_assets_select = $('#assigned_assets_select');
+                var selected = [];
+                add_asset.prop('disabled', true);
+                add_asset.click(function () {
+                    var selected_id = select.val();
+                    if (!selected.includes(selected_id)) {
+                        selected.push(selected_id);
+                    }
+                    updData();
+                    select.val(null).trigger('change');
+                    add_asset.prop('disabled', true);
+                });
 
-            // Get the search box within the dropdown or the selection
-            // Dropdown = single, Selection = multiple
-            var $search = $el.data('select2').dropdown.$search || $el.data('select2').selection.$search;
-            // This is undocumented and may change in the future
-
-            $search.val(term);
-            $search.trigger('input');
-        }
-
-        function scanCode(code){
-            console.log("scan: "+code);
-
-            // var $select = $($(this).data('target'));
-            // select2_search(select, code);
-
-
-            select.select2("trigger", "select", {
-                data: { asset_tag: code }
-            });
-
-            // select.val(code).trigger("change");
-            // select.trigger({
-            //     type: 'select2:select',
-            //     params: {
-            //         data: {
-            //             asset_tag:code
-            //         }
-            //     }
-            // });
-        }
+                select.on('select2:select', function (e) {
+                    // var data = e.params.data;
+                    // selected.push(data.id)
+                    // updData();
+                    add_asset.prop('disabled', false);
+                });
+                select.on('select2:unselect', function (e) {
+                    // var data = e.params.data;
+                    // console.log("select2:unselect");
+                    // console.log(data);
+                    // selected = selected.filter(item => item !== parseInt(data.id))
+                    // updData();
+                    add_asset.prop('disabled', true);
+                });
+                // select.on('select2:clear', function (e) {
+                //     // var data = e.params.data;
+                //     // console.log("select2:clear");
+                //     // console.log(data);
+                //     selected = [];
+                //     updData();
+                // });
 
 
+                function updData() {
+                    // console.log("upd");
+                    // console.log(selected);
+                    $("#assetsBulkTable").bootstrapTable("refresh", {
+                        "query": {
+                            "data": JSON.stringify(selected),
+                        },
+                        "silent": true,
+                    });
+                    $('#assigned_assets_select').empty();
+                    $.each(selected, function (i, item) {
+
+                        $('#assigned_assets_select').append($('<option>', {
+                            value: item,
+                            text: item,
+                            selected: true
+                        }));
+                    });
+                }
 
 
-        select.on('select2:select', function (e) {
-            var data = e.params.data;
-            selected.push(data.id)
-            updData();
-        });
-        select.on('select2:unselect', function (e) {
-            var data = e.params.data;
-            console.log("select2:unselect");
-            console.log(data);
-            selected = selected.filter(item => item !== parseInt(data.id))
-            updData();
-        });
-        select.on('select2:clear', function (e) {
-            // var data = e.params.data;
-            // console.log("select2:clear");
-            // console.log(data);
-            selected = [];
-            updData();
-        });
-        function auto_layout_keyboard( str ) {
-            var replacer = {
-                "й":"q", "ц":"w", "у":"e", "к":"r", "е":"t", "н":"y", "г":"u",
-                "ш":"i", "щ":"o", "з":"p", "х":"[", "ъ":"]", "ф":"a", "ы":"s",
-                "в":"d", "а":"f", "п":"g", "р":"h", "о":"j", "л":"k", "д":"l",
-                "ж":";", "э":"'", "я":"z", "ч":"x", "с":"c", "м":"v", "и":"b",
-                "т":"n", "ь":"m", "б":",", "ю":".", ".":"/"
-            };
+                // Enable scan events for the entire document
+                onScan.attachTo(document, {
+                    suffixKeyCodes: [13], // enter-key expected at the end of a scan
+                    // reactToPaste: true, // Compatibility to built-in scanners in paste-mode (as opposed to keyboard-mode)
+                    onScan: function (sCode, iQty) { // Alternative to document.addEventListener('scan')
+                        scanCode(auto_layout_keyboard(sCode));
+                    },
+                    // onKeyDetect: function(iKeyCode){ // output all potentially relevant key events - great for debugging!
+                    //     console.log('Pressed: ' + iKeyCode);
+                    // }
+                });
 
-            return str.replace(/[А-я/,.;\'\]\[]/g, function ( x ){
-                return x == x.toLowerCase() ? replacer[ x ] : replacer[ x.toLowerCase() ].toUpperCase();
-            });
-        }
+                function scanCode(code) {
+                    console.log("scan: " + code);
+
+                    // var $select = $($(this).data('target'));
+                    // select2_search(select, code);
 
 
-        function updData() {
-            console.log(selected);
-            $("#assetsBulkTable").bootstrapTable("refresh", {
-                "query": {
-                    "data": JSON.stringify(selected),
-                },
-                "silent": true,
-            })
-        }
-    </script>
+                    select.select2("trigger", "select", {
+                        data: {asset_tag: code}
+                    });
+
+                    // select.val(code).trigger("change");
+                    // select.trigger({
+                    //     type: 'select2:select',
+                    //     params: {
+                    //         data: {
+                    //             asset_tag:code
+                    //         }
+                    //     }
+                    // });
+                }
+
+                function auto_layout_keyboard(str) {
+                    var replacer = {
+                        "й": "q", "ц": "w", "у": "e", "к": "r", "е": "t", "н": "y", "г": "u",
+                        "ш": "i", "щ": "o", "з": "p", "х": "[", "ъ": "]", "ф": "a", "ы": "s",
+                        "в": "d", "а": "f", "п": "g", "р": "h", "о": "j", "л": "k", "д": "l",
+                        "ж": ";", "э": "'", "я": "z", "ч": "x", "с": "c", "м": "v", "и": "b",
+                        "т": "n", "ь": "m", "б": ",", "ю": ".", ".": "/"
+                    };
+
+                    return str.replace(/[А-я/,.;\'\]\[]/g, function (x) {
+                        return x == x.toLowerCase() ? replacer[x] : replacer[x.toLowerCase()].toUpperCase();
+                    });
+                }
+
+                // function select2_search($el, term) {
+                //     $el.select2('open');
+                //
+                //     // Get the search box within the dropdown or the selection
+                //     // Dropdown = single, Selection = multiple
+                //     var $search = $el.data('select2').dropdown.$search || $el.data('select2').selection.$search;
+                //     // This is undocumented and may change in the future
+                //
+                //     $search.val(term);
+                //     $search.trigger('input');
+                // }
+            </script>
 
 @stop
