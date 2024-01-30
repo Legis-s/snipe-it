c25734a5b12ae8016f7f9ef9fb5ca39791a66644
Sergey <okella92@gmail.com>
2024-01-30 23:36:37 +0200

    pre upd

--- resources/views/inventories/index.blade.php
+++ resources/views/inventories/index.blade.php
@@ -12,24 +12,28 @@
     <div class="row">
         <div class="col-md-12">
             <div class="box box-default">
-                <div class="box-body">
-                    <table
-                            data-click-to-select="true"
-                            data-columns="{{ \App\Presenters\InventoryPresenter::dataTableLayout() }}"
-                            data-cookie-id-table="inventoryTable"
-                            data-pagination="true"
-                            data-id-table="inventoryTable"
-                            data-search="true"
-                            data-side-pagination="server"
-                            data-show-columns="true"
-                            data-show-export="true"
-                            data-show-refresh="true"
-                            data-sort-order="asc"
-                            data-toolbar="#toolbar"
-                            id="inventoryTable"
-                            class="table table-striped snipe-table"
-                            data-url="{{ route('api.inventories.index') }}">
-                    </table>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="box-body">
+                            <table
+                                    data-click-to-select="true"
+                                    data-columns="{{ \App\Presenters\InventoryPresenter::dataTableLayout() }}"
+                                    data-cookie-id-table="inventoryTable"
+                                    data-pagination="true"
+                                    data-id-table="inventoryTable"
+                                    data-search="true"
+                                    data-side-pagination="server"
+                                    data-show-columns="true"
+                                    data-show-export="true"
+                                    data-show-refresh="true"
+                                    data-sort-order="asc"
+                                    data-toolbar="#toolbar"
+                                    id="inventoryTable"
+                                    class="table table-striped snipe-table"
+                                    data-url="{{ route('api.inventories.index') }}">
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
