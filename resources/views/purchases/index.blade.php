6a1d3699acd6fe82d5df414b2c93cfbe38314e47
Sergey <okella92@gmail.com>
2025-12-22 20:09:14 +0200

    upd

--- resources/views/purchases/index.blade.php
+++ resources/views/purchases/index.blade.php
@@ -14,51 +14,42 @@
             <div class="box box-default">
                 <div class="box-body">
                     <div class="table-responsive">
-                    <div id="toolbar">
-                        @isset($users)
-                            <select class="js-select-user">
-                                <option></option>
-                                @foreach($users as $user)
-                                    <option value="{{ $user->id }}">{{ $user->last_name }} {{ $user->first_name }}</option>
-                                @endforeach
+                        <div id="toolbar">
+                            @isset($users)
+                                <select id="js-select-user">
+                                    <option value="0" ></option>
+                                    @foreach($users as $user)
+                                        <option value="{{ $user->id }}">{{ $user->last_name }} {{ $user->first_name }}</option>
+                                    @endforeach
+                                </select>
+                            @endisset
+                            <select id="js-select-status">
+                                <option value="0" ></option>
+                                <option value="inventory">В процессе инвентаризации</option>
+                                <option value="in_payment">В оплате</option>
+                                <option value="review">В процессе проверки</option>
+                                <option value="finished">Завершено</option>
+                                <option value="rejected">Отклонено</option>
+                                <option value="paid">Оплачено</option>
+                                <option value="inprogress">>На согласовании</option>
                             </select>
-                        @endisset
-                        <select class="js-select-status">
-                            <option></option>
-                            <option value="inventory">В процессе инвентаризации</option>
-                            <option value="in_payment">В оплате</option>
-                            <option value="review">В процессе проверки</option>
-                            <option value="finished">Завершено</option>
-                            <option value="rejected">Отклонено</option>
-                            <option value="paid">Оплачено</option>
-                            <option value="inprogress">>На согласовании</option>
-                        </select>
-
-                        @isset($suppliers)
-                            <select class="js-select-supplier">
-                                <option></option>
-                                @foreach($suppliers as $supplier)
-                                    <option value="{{ $supplier->id }}">{{ $supplier->name }}</option>
-                                @endforeach
-                            </select>
-                        @endisset
-                    </div>
-                    <table
-                            data-columns="{{ \App\Presenters\PurchasePresenter::dataTableLayout() }}"
-                            data-cookie-id-table="purchasesTable"
-                            data-id-table="purchasesTable"
-                            data-side-pagination="server"
-                            data-sort-order="desc"
-                            data-toolbar="#toolbar"
-                            data-buttons="purchaseButtons"
-                            id="purchasesTable"
-                            class="table table-striped snipe-table"
-                            data-url="{{ route('api.purchases.index') }}"
-                            data-export-options='{
+                        </div>
+                        <table
+                                data-columns="{{ \App\Presenters\PurchasePresenter::dataTableLayout() }}"
+                                data-cookie-id-table="purchasesTable"
+                                data-id-table="purchasesTable"
+                                data-side-pagination="server"
+                                data-sort-order="desc"
+                                data-toolbar="#toolbar"
+                                data-buttons="purchaseButtons"
+                                id="purchasesTable"
+                                class="table table-striped snipe-table"
+                                data-url="{{ route('api.purchases.index') }}"
+                                data-export-options='{
               "fileName": "export-purchases-{{ date('Y-m-d') }}",
               "ignoreColumn": ["actions","image","change","checkbox","checkincheckout","icon"]
               }'>
-                    </table>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -70,57 +61,48 @@
 @section('moar_scripts')
     @include ('partials.bootstrap-table', ['exportFile' => 'purchases-export', 'search' => true])
     <script nonce="{{ csrf_token() }}">
-        $(function ()  {
-            var query_holder = {};
 
-            $('.js-select-user').select2({
+        $(function () {
+            const $table = $('#purchasesTable');
+            const $select_user = $('#js-select-user');
+            const $select_status = $('#js-select-status');
+            $table.bootstrapTable('refreshOptions', {
+                queryParams: params => {
+                    const user = $select_user.val();
+                    const status =  $select_status.val();
+                    if (parseInt(user)>0){
+                        params.created_by = parseInt(user);
+                    }else{
+                        delete params.created_by;
+                    }
+                    if (status?.length > 3) {
+                        params.status = status;
+                    } else {
+                        delete params.status;
+                    }
+
+                    console.log(params);
+                    return params
+                }
+            })
+
+            $select_user.select2({
                 placeholder: 'Выберите пользователя',
                 allowClear: true,
                 width: "200px"
+            }).on('select2:select', function (e) {
+                $table.bootstrapTable('refresh');
+            }).on('select2:clear', function (e) {
+                $table.bootstrapTable('refresh');
             });
-            $('.js-select-status').select2({
+            $select_status.select2({
                 placeholder: 'Выберите статус',
                 allowClear: true,
                 width: "200px"
-            });
-            $('.js-select-supplier').select2({
-                placeholder: 'Выберите поставщика',
-                allowClear: true,
-                width: "200px"
-            });
-            $('.js-select-user').on('select2:select', function (e) {
-                $('.js-select-status').val(null).trigger('change');
-                $('.js-select-supplier').val(null).trigger('change');
-                var data = e.params.data;
-                $('#purchasesTable').bootstrapTable('refresh', {
-                    query: {user_id: data.id}
-                })
-            });
-            $('.js-select-user').on('select2:clear', function (e) {
-                $('#purchasesTable').bootstrapTable('refresh')
-            });
-            $('.js-select-status').on('select2:select', function (e) {
-                $('.js-select-user').val(null).trigger('change');
-                $('.js-select-supplier').val(null).trigger('change');
-                var data = e.params.data;
-                $('#purchasesTable').bootstrapTable('refresh', {
-                    query: {status: data.id}
-                })
-            });
-            $('.js-select-status').on('select2:clear', function (e) {
-
-                $('#purchasesTable').bootstrapTable('refresh')
-            });
-            $('.js-select-supplier').on('select2:select', function (e) {
-                $('.js-select-user').val(null).trigger('change');
-                $('.js-select-status').val(null).trigger('change');
-                var data = e.params.data;
-                $('#purchasesTable').bootstrapTable('refresh', {
-                    query: {supplier: data.id}
-                })
-            });
-            $('.js-select-supplier').on('select2:clear', function (e) {
-                $('#purchasesTable').bootstrapTable('refresh')
+            }).on('select2:select', function (e) {
+                $table.bootstrapTable('refresh');
+            }).on('select2:clear', function (e) {
+                $table.bootstrapTable('refresh');
             });
         });
     </script>
